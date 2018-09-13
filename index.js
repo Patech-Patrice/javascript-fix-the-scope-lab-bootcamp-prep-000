9c5069fb423a1f31a54de15fa2efc71820d9737a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-13 18:11:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ function yourAnimal() {
 
 function add2(n) {
     var add2=(n+'2');
-  return  (add( n + '2'));
-  // Feel free to move things around!
-  const two = 2;
+  const two = ('2');
 }
 
 var funkyFunction = function() {
